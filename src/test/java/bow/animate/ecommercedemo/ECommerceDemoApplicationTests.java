380826714f3131424d91131a957911b5d425396d
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-02-20 20:29:06 +0330

    firtst

---
+++ src/test/java/bow/animate/ecommercedemo/ECommerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bow.animate.ecommercedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
